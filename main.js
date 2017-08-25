9b949888a9f8b180d1228542f1395964a5e6ba9e
stojan.milosev <stojan.milosev@yahoo.com>
2017-08-25 12:06:21 +0200

    manje CSS izmene, dodati komentari

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
+//definisanje promenljive elementa pomocu klase
 var drop = document.getElementsByClassName("drop");
+//funkcija menja prisustvo klase 'show' koja utice na vidljiost elementa
 function show(){
      document.getElementById("content").classList.toggle("show");
 }
